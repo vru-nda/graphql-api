5d23418bc11ff8c4625a85f5307e02d17a54f923
Vrunda Joshi <vrunda1002@gmail.com>
2021-08-30 16:41:44 +0530

    inital commit

--- NodeProject/app.js
+++ NodeProject/app.js
@@ -6,9 +6,10 @@ const mongoose = require("mongoose");
 const multer = require("multer");
 const { v4: uuidv4 } = require("uuid");
 require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
 
-const feedRoutes = require("./routes/feed");
-const authRoutes = require("./routes/auth");
+const graphqlSchema = require("./graphql/schema");
+const graphqlResolver = require("./graphql/resolvers");
 
 const app = express();
 
@@ -48,8 +49,13 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/feed", feedRoutes);
-app.use("/auth", authRoutes);
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+  })
+);
 
 app.use((error, req, res, next) => {
   console.log(error);
@@ -61,10 +67,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(process.env.MONGODB_URI)
   .then((result) => {
-    const server = app.listen(8080);
-    const io = require("./socket").init(server);
-    io.on("connection", (socket) => {
-      console.log("Client connected");
-    });
+    app.listen(8080);
+    console.log("Connected");
   })
   .catch((err) => console.log(err));
---
+++ NodeProject/graphql/resolvers.js
@@ -0,0 +1,8 @@
+module.exports = {
+  hello() {
+    return {
+      text: "Hello there",
+      views: 1234,
+    };
+  },
+};
