6b974a544946d88d6fe3f3dac9e2bec0fb662e80
Vrunda Joshi <vrunda1002@gmail.com>
2021-08-30 15:10:17 +0530

    REST and websocket

---
+++ NodeProject/app.js
@@ -0,0 +1,70 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
+require("dotenv").config();
+
+const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
+
+const app = express();
+
+const fileStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "images");
+  },
+  filename: function (req, file, cb) {
+    cb(null, uuidv4() + "-" + file.originalname);
+  },
+});
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/png"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.use(bodyParser.json());
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then((result) => {
+    const server = app.listen(8080);
+    const io = require("./socket").init(server);
+    io.on("connection", (socket) => {
+      console.log("Client connected");
+    });
+  })
+  .catch((err) => console.log(err));
---
+++ README.md
@@ -0,0 +1,3 @@
+# rest-with-node
+- It's a social media feed-like web application to demonstrate the working of REST API using Node JS and React. 
+- Also, the websocket - Socket.io is used to provide a more real-time experience.
